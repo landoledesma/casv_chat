3b5e9a8d7721b315805e8790b6c962825d0674fa
Lando Ledesma <lando_ledesma@outlook.com>
2023-10-11 12:50:02 -0600

    creando funcion para cargar datos

--- conversation.py
+++ conversation.py
@@ -21,14 +21,21 @@ def load_llm():
         )
     return llm
 
+def load_data(upload_file):
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(upload_file.getvalue())
+        tmp_file_path = tmp_file.name
+    loader = CSVLoader(file_path=tmp_file_path,encoding="utf-8",csv_args={'delimiter':','})
+    data = loader.load()
+    return data
+
 def conversational_chat(chain,query,history):
     result = chain({"question":query,"chat_history":history})
     return result["answer"]
 
-def process_data(data):
+def process_data(data,llm):
     embeddings = OpenAIEmbeddings()
     db = FAISS.from_documents(data, embeddings)
     db.save_local(DB_FAISS_PATH)
-    llm = load_llm()
     chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
     return chain
